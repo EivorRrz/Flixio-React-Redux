1e5c4b074b205fe1206ae41c1add3b5e6af96aed
Amit Mishra <eivorftw234@gmail.com>
2024-06-02 00:37:08 +0530

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <ul className="menuItems">
+                    <li className="menuItem">Terms Of Use</li>
+                    <li className="menuItem">Privacy-Policy</li>
+                    <li className="menuItem">About</li>
+                    <li className="menuItem">Blog</li>
+                    <li className="menuItem">FAQ</li>
+                </ul>
+                <div className="infoText">
+                    
+                </div>
+              
+            </ContentWrapper>
+        </footer>
+    );
+};
+
+export default Footer;
